b88d65d1c785ddc28878808a285e40da51e0976f
harrines <harrines0920@gmail.com>
2025-10-08 14:59:56 +0530

    leave request2

--- Attendance-user/frontend/src/components/Leaverequest.jsx
+++ Attendance-user/frontend/src/components/Leaverequest.jsx
@@ -124,7 +124,7 @@ const LeaveRequest = () => {
 
   const leaverequestapi = (newLeave) => {
     setIsApplying(true);
-    const userid = LS.get("userid");
+    const userid = LS.get("id");
     let currentDate = new Date();
     let time = currentDate.toLocaleTimeString().toString();
 
